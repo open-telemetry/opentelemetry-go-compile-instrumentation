aa4b303fa8cebdb44379cb4e8275fba49ebd9d5e
Przemek Delewski <przemyslaw_delewski@o2.pl>
2025-06-25 21:23:37 +0200

    Fixing missing dependencies by manual go mod tidy

--- build.sh
+++ build.sh
@@ -3,5 +3,10 @@ go build -a -o otel
 TOOL=$(pwd)/otel
 # compile-time instrumentation via toolexec
 cd demo
+# TODO
+# The 'tidy' step should ideally be invoked by the tool as the first stage.
+# However, since this is not implemented yet, it must be run separately here
+# as a standalone command to resolve all dependencies.
+go mod tidy
 go build -a -work -toolexec=$TOOL -o demo .
 ./demo
