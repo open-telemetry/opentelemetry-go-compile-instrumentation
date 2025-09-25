df993c182e70d2e138219aa6e8d5a498ff161c2a
Yi Yang <qingfeng.yy@alibaba-inc.com>
2025-09-25 10:07:35 +0800

    underscore;merge file

--- tool/internal/instrument/match.go
+++ tool/internal/instrument/match.go
@@ -4,11 +4,35 @@
 package instrument
 
 import (
+	"encoding/json"
+	"os"
 	"strings"
 
+	"github.com/open-telemetry/opentelemetry-go-compile-instrumentation/tool/ex"
 	"github.com/open-telemetry/opentelemetry-go-compile-instrumentation/tool/internal/rule"
 	"github.com/open-telemetry/opentelemetry-go-compile-instrumentation/tool/util"
 )
+
+// load loads the matched rules from the build temp directory.
+// TODO: Shared memory across all sub-processes is possible
+func (ip *InstrumentPhase) load() ([]*rule.InstFuncRule, error) {
+	f := util.GetMatchedRuleFile()
+	content, err := os.ReadFile(f)
+	if err != nil {
+		return nil, ex.Errorf(err, "failed to read file %s", f)
+	}
+	if len(content) == 0 {
+		return nil, nil
+	}
+
+	rules := make([]*rule.InstFuncRule, 0)
+	err = json.Unmarshal(content, &rules)
+	if err != nil {
+		return nil, ex.Errorf(err, "failed to unmarshal rules from file %s", f)
+	}
+	ip.Debug("Loaded matched rules", "rules", rules)
+	return rules, nil
+}
 
 // match matches the rules with the compile command.
 func (ip *InstrumentPhase) match(args []string) ([]*rule.InstFuncRule, error) {
