016892e0affb0174933ef41f44bf3c3e42be11f1
Przemek Delewski <przemyslaw_delewski@o2.pl>
2025-11-12 11:01:16 +0100

    fix: removing go:linkname

--- pkg/instrumentation/grpc/server_hook.go
+++ pkg/instrumentation/grpc/server_hook.go
@@ -6,12 +6,10 @@ package grpc
 import (
 	"fmt"
 	"net"
-	_ "unsafe"
 
 	"github.com/open-telemetry/opentelemetry-go-compile-instrumentation/pkg/inst"
 )
 
-//go:linkname BeforeServe google.golang.org/grpc.BeforeServe
 func BeforeServe(ictx inst.HookContext, _ interface{}, lis net.Listener) {
 	fmt.Println("BeforeServe")
 	// TODO: Implement the real server hook logic here
