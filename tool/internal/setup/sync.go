0d1c4e2a2b4060a60723680c2a5813f3260bc4c4
txabman42 <x42.martinez@gmail.com>
2025-11-28 21:23:16 +0100

    extract pkgs from args

--- tool/internal/setup/sync.go
+++ tool/internal/setup/sync.go
@@ -41,8 +41,8 @@ func writeGoMod(gomod string, modfile *modfile.File) error {
 	return nil
 }
 
-func runModTidy(ctx context.Context, moduleDir string) error {
-	return util.RunCmdInDir(ctx, moduleDir, "go", "mod", "tidy")
+func runModTidy(ctx context.Context) error {
+	return util.RunCmd(ctx, "go", "mod", "tidy")
 }
 
 func addReplace(modfile *modfile.File, path, rpath string) (bool, error) {
@@ -122,7 +122,7 @@ func (sp *SetupPhase) syncDeps(ctx context.Context, matched []*rule.InstRuleSet,
 	// In a matching rule, such as InstFuncRule, the hook code is defined in a
 	// separate module. Since this module is local, we need to add a replace
 	// directive in go.mod to point the module name to its local path.
-	goModFile := filepath.Join(moduleDir, "go.mod")
+	const goModFile = "go.mod"
 	modfile, err := parseGoMod(goModFile)
 	if err != nil {
 		return err
@@ -182,7 +182,7 @@ func (sp *SetupPhase) syncDeps(ctx context.Context, matched []*rule.InstRuleSet,
 		if err != nil {
 			return err
 		}
-		err = runModTidy(ctx, moduleDir)
+		err = runModTidy(ctx)
 		if err != nil {
 			return err
 		}
