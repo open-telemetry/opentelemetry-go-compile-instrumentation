bc3576371a3d26d27202e95d0f1cb03f879fb914
Dario Castañé <dario.castane@datadoghq.com>
2026-01-21 18:38:15 +0100

    fix: apply license header

--- tool/internal/instrument/template/placeholder.go
+++ tool/internal/instrument/template/placeholder.go
@@ -1,7 +1,5 @@
-// Unless explicitly stated otherwise all files in this repository are licensed
-// under the Apache License Version 2.0.
-// This product includes software developed at Datadog (https://www.datadoghq.com/).
-// Copyright 2023-present Datadog, Inc.
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
 
 package template
 
