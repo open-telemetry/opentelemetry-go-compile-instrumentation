0972c1f3f405f771baa5a27253892c020c26e60e
Tushar Gupta <tushar.197712@gmail.com>
2026-02-25 12:46:51 +0530

    fixed messaging operations

--- pkg/instrumentation/segmentio/semconv/client.go
+++ pkg/instrumentation/segmentio/semconv/client.go
@@ -4,8 +4,6 @@
 package semconv
 
 import (
-	"strconv"
-
 	"go.opentelemetry.io/otel/attribute"
 	semconv "go.opentelemetry.io/otel/semconv/v1.37.0"
 )
@@ -16,45 +14,26 @@ const (
 	KafkaOperationPublish KafkaOperation = "publish"
 	KafkaOperationReceive KafkaOperation = "receive"
 	KafkaOperationProcess KafkaOperation = "process"
-	KafkaOperationSettle  KafkaOperation = "settle"
+	KafkaOperationAck     KafkaOperation = "ack"
 )
 
 type KafkaRequest struct {
 	Topic           string
 	Operation       KafkaOperation
 	Partition       string
-	Offset          string
+	Offset          int
 	ConsumerGroupID string
 	MessageKey      string
 }
 
 func KafkaRequestTraceAttrs(req KafkaRequest) []attribute.KeyValue {
-	attrs := []attribute.KeyValue{
+	return []attribute.KeyValue{
 		semconv.MessagingSystemKafka,
 		semconv.MessagingDestinationName(req.Topic),
 		semconv.MessagingDestinationPartitionIDKey.String(req.Partition),
 		semconv.MessagingOperationName(string(req.Operation)),
+		semconv.MessagingKafkaOffset(req.Offset),
+		semconv.MessagingConsumerGroupName(req.ConsumerGroupID),
+		semconv.MessagingKafkaMessageKey(req.MessageKey),
 	}
-
-	if req.MessageKey != "" {
-		attrs = append(attrs, semconv.MessagingKafkaMessageKey(req.MessageKey))
-	}
-
-	isConsumer := req.Operation == KafkaOperationReceive ||
-		req.Operation == KafkaOperationProcess ||
-		req.Operation == KafkaOperationSettle ||
-		req.Operation == KafkaOperationPublish
-
-	if isConsumer {
-		if req.Offset != "" {
-			if offset, err := strconv.Atoi(req.Offset); err == nil {
-				attrs = append(attrs, semconv.MessagingKafkaOffset(offset))
-			}
-		}
-		if req.ConsumerGroupID != "" {
-			attrs = append(attrs, semconv.MessagingConsumerGroupName(req.ConsumerGroupID))
-		}
-	}
-
-	return attrs
 }
