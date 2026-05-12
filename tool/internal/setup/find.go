3b2c5b7b16b4bd3ae7bfee8c13ba028a75abdaf1
Azhar Momin <93782497+amazingakai@users.noreply.github.com>
2026-05-12 11:20:06 +0530

    apply suggestion
    Co-authored-by: Xabier Martinez <39015378+txabman42@users.noreply.github.com>

--- tool/internal/setup/find.go
+++ tool/internal/setup/find.go
@@ -84,9 +84,9 @@ func (sp *SetupPhase) listBuildPlan(ctx context.Context, cmdArgs []string) ([]st
 	}
 	defer buildPlanLog.Close()
 	// The full build command is: "go build/install -a -x -n  {...}"
-	buildArgsLen := 4
-	args := make([]string, 0, buildArgsLen+len(cmdArgs))
-	args = append(args, "build", "-a", "-x", "-n") // build -a -x -n
+	prefix := []string{"build", "-a", "-x", "-n"}
+	args := make([]string, 0, len(prefix)+len(cmdArgs))
+	args = append(args, prefix...)
 	args = append(args, cmdArgs...)                // args from original build/install or setup command
 	sp.Info("go build command", "args", args)
 
