50832c5978d1e069da354bf6aa169fdbeab2e5ef
txabman42 <x42.martinez@gmail.com>
2026-05-14 17:58:40 +0200

    add all rule types for external wrapper test

--- tool/internal/instrument/testdata/golden/all-rule-external-wrapper/source.go
+++ tool/internal/instrument/testdata/golden/all-rule-external-wrapper/source.go
@@ -5,6 +5,19 @@ package main
 
 import "unsafe"
 
+type T struct{}
+
+var GlobalVar interface{} = "original"
+
+func RawFunc() {
+	println("raw func body")
+}
+
+//otelc:external-log
+func DirectiveFunc() {
+	println("directive func body")
+}
+
 func CallSizeof() {
 	x := 42
 	size := unsafe.Sizeof(x)
--- tool/internal/instrument/testdata/golden/call-rule-external-wrapper/helpers/wrapper/wrapper.go
+++
@@ -1,8 +0,0 @@
-// Copyright The OpenTelemetry Authors
-// SPDX-License-Identifier: Apache-2.0
-
-package wrapper
-
-func Wrapper(size uintptr) uintptr {
-	return size
-}
