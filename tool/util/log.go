388e9351743734f9ed9faeab696688ef70600b1b
ADITYA-CODE-SOURCE <adityavishe67@gmail.com>
2026-05-20 08:23:23 +0530

    fix(cmd): make --work-dir control the full otelc workspace

--- tool/util/log.go
+++ tool/util/log.go
@@ -5,13 +5,19 @@ package util
 
 import (
 	"context"
+	"io"
 	"log/slog"
 )
 
 type contextKeyLogger struct{}
+type contextKeyLogWriter struct{}
 
 func ContextWithLogger(ctx context.Context, logger *slog.Logger) context.Context {
 	return context.WithValue(ctx, contextKeyLogger{}, logger)
+}
+
+func ContextWithLogWriter(ctx context.Context, writer io.Closer) context.Context {
+	return context.WithValue(ctx, contextKeyLogWriter{}, writer)
 }
 
 func LoggerFromContext(ctx context.Context) *slog.Logger {
@@ -20,4 +26,12 @@ func LoggerFromContext(ctx context.Context) *slog.Logger {
 		return slog.Default()
 	}
 	return logger
+}
+
+func LogWriterFromContext(ctx context.Context) io.Closer {
+	writer, ok := ctx.Value(contextKeyLogWriter{}).(io.Closer)
+	if !ok {
+		return nil
+	}
+	return writer
 }
