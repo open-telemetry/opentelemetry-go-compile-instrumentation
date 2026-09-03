5b6d0980a58d007f56c567ae4b0556ebbb9457b8
Milen Iliev <mr.milen.i@gmail.com>
2026-09-03 14:09:11 +0300

    integration tests

--- test/integration/logs_test.go
+++ test/integration/logs_test.go
@@ -6,7 +6,9 @@
 package test
 
 import (
+	"encoding/json"
 	"regexp"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -96,18 +98,33 @@ func TestLogsZerolog(t *testing.T) {
 		"logger print message",
 		"logger printf message",
 		"logger println message",
-		"fatal message",
 		"panic message",
-	}
-	for _, msg := range zerologMessages {
-		require.Contains(t, output, msg, "Expected zerolog message: %s", msg)
+		"fatal message",
 	}
 
-	traceIDPattern := regexp.MustCompile(`"trace_id":"[a-f0-9]{32}"`)
-	matches := traceIDPattern.FindAllString(output, -1)
-	require.NotEmpty(t, matches, "Expected trace_id to be injected into zerolog messages")
+	type zerologEntry struct {
+		Message string `json:"message"`
+		TraceID string `json:"trace_id"`
+		SpanID  string `json:"span_id"`
+	}
+	
+	entries := make(map[string]zerologEntry)
+	for line := range strings.Lines(output) {
+		var entry zerologEntry
+		err := json.Unmarshal([]byte(line), &entry)
+		require.Nil(t, err, "Failed to unmarshal zerolog entry: %s", line)
 
-	spanIDPattern := regexp.MustCompile(`"span_id":"[a-f0-9]{16}"`)
-	spanMatches := spanIDPattern.FindAllString(output, -1)
-	require.NotEmpty(t, spanMatches, "Expected span_id to be injected into zerolog messages")
+		entries[strings.TrimSuffix(entry.Message, "\n")] = entry
+	}
+
+	loggers := []string{"loggerFromNew", "loggerFromInstantiated"}
+
+	for _, logger := range loggers {
+		for _, msg := range zerologMessages {
+			entry, ok := entries[msg+" "+logger]
+			require.True(t, ok, "Expected zerolog message: %s %s", msg, logger)
+			require.NotZero(t, entry.TraceID, "Expected trace_id on zerolog message: %s %s", msg, logger)
+			require.NotZero(t, entry.SpanID, "Expected span_id on zerolog message: %s %s", msg, logger)
+		}
+	}
 }
